bfda00648076d783f914e20c494ae7247e8c2458
kang.yang <kang.yang@b-and-qchina.com>
2020-04-02 00:30:33 +0800

    spring-web demo项目

---
+++ spring-boot-web/src/main/java/com/aking/springbootweb/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.aking.springbootweb.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName HelloController
+ * @Description
+ * @Author yangkang
+ * @Date 2020/4/2 0:08
+ * @Version 1.0
+ **/
+@RestController
+public class HelloController {
+    @RequestMapping(path = {"/helloSpringBoot"})
+    public String HelloSpring (){
+        System.out.println("hello spring boot");
+        return "hello spring boot";
+    }
+}
