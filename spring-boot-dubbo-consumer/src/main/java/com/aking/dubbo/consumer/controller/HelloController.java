c93b8557a75cd059548bd1f6611eedeee212d2d4
kang.yang <kang.yang@b-and-qchina.com>
2020-07-03 16:12:49 +0800

    dubbo

--- spring-boot-dubbo-consumer/src/main/java/com/aking/dubbo/consumer/controller/HelloController.java
+++ spring-boot-dubbo-consumer/src/main/java/com/aking/dubbo/consumer/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.aking.dubbo.consumer.controller;
 
 import com.aking.dubbo.api.service.HelloService;
 import com.alibaba.dubbo.config.annotation.Reference;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
  * @Version 1.0
  **/
 @RestController
+@Slf4j
 public class HelloController {
-    @Reference(version = "${dubbo.service.version}",group = "${dubbo.provider.group}")
+    @Reference(version = "${dubbo.service.version}", group = "${dubbo.provider.group}")
     private HelloService helloService;
 
     @RequestMapping("/hello.do")
-    public String sayHello() {
-        String s = helloService.sayHello("Python大星");
-        return s;
+    public String sayHello() throws InterruptedException {
+        // 测试 zk 宕机，服务消费情况
+        log.info("第 1 次消费：[{}]",
+                helloService.sayHello("Python大星"));
+        Thread.sleep(1000 * 20);
+        log.info("第 2 次消费：[{}]",
+                helloService.sayHello("Python小星"));
+        return "success";
     }
 }
